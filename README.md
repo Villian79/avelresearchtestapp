a734dbc800de24bb4a35df1e4fe6541a5a541482
Igor Vilinchuk <ivilinchuk@gmail.com>
2018-08-24 19:50:04 +0800

    Create config.ini, move comments to README.md

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # avelresearchtestapp
-Sample codebase to test FB Graph API
+## Sample codebase to test FB Graph API
+
+This is the script to access data from the Facebook user profile
+In order to use the Graph API we should register our app on [developers.facebook.com](https://developers.facebook.com)
+After the registration and confirmation process is finished we can obtain `ACCESS TOKEN`
+This script allows to extract FB User's public personal data and public posts and creates a text file where all the extracted data is stored.
+
+---
+### Running the script
+
+Make sure to run this script in Python.3.x environment
+
+Add configuration details in `config.ini` file:
+- `access_token` - value of the access token issued by Graph API
+- `user_id` - target User's FB id
+
+Install 3rd party Python module [Facepy](https://github.com/jgorset/facepy)
+To do so run this command in your command line: `pip install facepy`
+
+Note: 
+At the moment owner of the current app has access only to his personal data
+To get full access to FB Graph API endpoints potentional real app owner has to submit additional
+information to FB. After review permissions to use certain query endpoints will be granted by FB.
+
+---
+
+Enter directory where the file user_posts.py is located
+Run command `python user_posts.py` from within that directory
+
+
+---
+### Usage
+
+On the first run of the script a new userdata.txt file is created in cwd where target User's data is pushed
+
+On each next run of the script the target FB User's data is collected and checked agains data existing in userdata.txt. If new posts exist - they are automatically added
+
+While running the script we send GET requests to various Graph API endpoints using Facepy Graph API module.
+Almost all requests are passed to the `graph.facebook.com` host URL.
+
+Reading operations almost always begin with a node. A node is an individual object with a unique ID.
+`GET https://graph.facebook.com/820882001277849`
+Result of such GET request is as follows:
+
+```
+{
+  "name": "Coca-Cola",
+  "id": "820882001277849"
+}
+```
+
+In this app we used two main GET requests paths:
+- `GET https://graph.facebook.com/XXXXXXXXXXXXXX?fields=id,name,location,hometown,email` - extracting User's public personal information
+- `GET https://graph.facebook.com/XXXXXXXXXXXXXX?fields=feed.order(chronological).limit(100)` - extracting User's latest 100 public posts
+
+
+
--- user_posts.py
+++ user_posts.py
@@ -1,36 +1,32 @@
 #! python3
-#This is the script to access data from the Facebook user profile
-#In order to use the Graph API we should register our app on https://developers.facebook.com
-#After the registration and confirmation process is finished we can obtain 
-
-#To run this script make sure to install 3rd party Python module Facepy: https://github.com/jgorset/facepy
-#To do so run this command in your command line: pip install facepy
 
 from facepy import GraphAPI
 import os, re
 from pprint import pprint
+from configparser import ConfigParser
+
+#Get config data
+if __name__ == '__main__':
+  cfg = ConfigParser()
+  cfg.read('config.ini')
+  ACCESS_TOKEN = cfg.get('App','access_token')
+  FILE_PATH = cfg.get('App', 'file_path')
+  USER_ID = cfg.get('User', 'id')
 
 # Initialize the Graph API with a valid access token (optional, but will allow you to do all sorts of fun stuff).
-graph = GraphAPI('EAAeZATZBKTs2oBAC3i7OVytZBLfYVIoUQYuucMas2F0SoBuyqDxkfo9tv0G7w44wZApCi0GE2ZAgpZCzkTqUblCctdaE76BcMl5FLku0UZAaxcKHbpoMwfWVN5ctIL8YK2PELIWALzZCcT4My8bwLqmhJtlkRrbcaKcZD')
-
+graph = GraphAPI(ACCESS_TOKEN)
 # Get the User profile
-# Numerical '2091816324224785' part of the query is the User Facebook id
-# At the moment owner of the current app has access only to his personal data
-# To get full access to FB Graph API endpoints potentional real app owner has to submit additional
-# information to FB. After review permissions to use certain query endpoints will be granted by FB.
-
-profile = graph.get('2091816324224785?fields=id,name,location,hometown,email')
+profile = graph.get(USER_ID + '?fields=id,name,location,hometown,email')
 
 # Get my latest posts 100 posts
-feed = graph.get('2091816324224785?fields=feed.order(chronological).limit(100)')
+feed = graph.get(USER_ID + '?fields=feed.order(chronological).limit(100)')
 
 #********************Functions to run the script*************************************************
 
 #--------------------Start of createfile() function----------------------------------------------
 #Creating userdata.txt file in case it is not in cwd
 def createfile():
-  #Writing User data and Latest posts to a .txt file
-  user_data_file = open('./userdata.txt', 'w')
+  user_data_file = open(os.getcwd() + FILE_PATH, 'w')
   user_data_file.write(
     'Name: ' + profile['name'] +
     '\nFB User ID: ' + profile['id'] +
@@ -39,13 +35,11 @@ def createfile():
     '\nCurrent Location: ' + profile['location']['name'] +
     '\n' + ('-' * 35))
 
-  #Check all posts from FB and choose those that include 'message' key
   user_posts = []
   for post in feed['feed']['data']:
     if 'message' in post:
       user_posts.append(post)
 
-  #Write chosen posts to userdata.txt file
   for post in user_posts:
     user_data_file.write(
       '\nDate&Time created: ' + post['created_time'] +
@@ -55,7 +49,6 @@ def createfile():
 
   user_data_file.close()
 
-  #Print out results to console
   print(
     'Name: ' + profile['name'] +
     '\nFB User ID: ' + profile['id'] +
@@ -74,17 +67,13 @@ def createfile():
 #--------------------------------Start of manage_posts() function----------------------------------------
 
 def manage_posts():
-  #Open userdata.txt to read it's contents
-  f = open('./userdata.txt', 'r')
+  f = open(FILE_PATH, 'r')
   content = f.read()
   f.close()
 
-  #Check all posts from FB and choose those that have 'message' key
   for post in feed['feed']['data']:
     if 'message' in post:
-      #Check if the post with a given 'id' exists in the userdata.txt file using RegEx
       id_user_regex = re.compile(post['id'])
-      #If the post is not included in the userdata.txt file - create a new entry
       if not id_user_regex.search(content):
         f = open('./userdata.txt', 'r')
         lines_list = f.readlines()
@@ -106,10 +95,6 @@ def manage_posts():
 #********************************End of Functions Section************************************************
 
 
-
-#Check if the userdata.txt file to store our information exists in the cwd
-#If id doesn't exist - Create one
-#If it exists - Manage data in it
 if not os.path.isfile('./userdata.txt'):
   createfile()
   print('Successfully created userdata.txt file')
